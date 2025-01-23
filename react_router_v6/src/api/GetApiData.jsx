915f8083daa92ef156f4a216894b5f4adbbdd0c7
akshay.thappar@inputzero.com <Test@123>
2025-01-23 23:41:54 +0530

    understanding env variable

--- react_router_v6/src/api/GetApiData.jsx
+++ react_router_v6/src/api/GetApiData.jsx
@@ -2,7 +2,7 @@
 
 export const getMoviesData = async() =>{
     try{
-        const response = await fetch("https://www.omdbapi.com/?i=tt3896198&apikey=b7477d51&s=titanic&page=1");
+        const response = await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${import.meta.env.VITE_API_KEY}&s=titanic&page=1`);
         const data = await response.json();
         // console.log(data);
         return data;
